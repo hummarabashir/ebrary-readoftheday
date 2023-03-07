d338f189713b8f0207e8a16f09d52efab99ca879
Hummara Bashir <hummarabashir@gmail.com>
2023-03-07 07:22:21 -0500

    addtoorder

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,38 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
-
+import base from "../"
 
 class App extends React.Component {
     state = {
         fishes: {},
         order: {}
     };
+
+    // componentDidMount() {
+    //     const { params } = this.props.match;
+    //     const localStorageRef = localStorage.getItem();
+    // }
+    // componentDidUpdate() {
+    //     localStorage.setItem(
+    //         this.props.match.params.storeId,
+    //         JSON.stringify(this.state.order)
+    //     );
+    // }
+    // componentWillUnmount() {
+        
+    // }
+
     addFish = fish => {
         const fishes = { ...this.state.fishes };
         fishes[`fish${Date.now()}`] = fish;
         this.setState({ fishes });
     };
     loadSampleFishes = () => {
-        this.setState({ fishes: sampleFishes});
+        this.setState({ fishes: sampleFishes });
     }
     addToOrder = key => {
-        const order = { ...order };
+        const order = { ...this.state.order };
         order[key] = order[key] + 1 || 1;
         this.setState({ order });
     };
