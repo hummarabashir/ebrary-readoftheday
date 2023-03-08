5bc2e424373cd739c0995d1840f0a19d5a100847
Hummara Bashir <hummarabashir@gmail.com>
2023-03-08 13:45:07 -0500

    stepped

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp(
     {
-        apiKey: "AIzaSyB0aSglTq1iWOWPkNEcka8qPN7M1LcR3kU",
-        authDomain: "ebrary-readoftheday-123.firebaseapp.com",
-        databaseURL: "https://ebrary-readoftheday-123-default-rtdb.firebaseio.com"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: ""
       }
 );
 
